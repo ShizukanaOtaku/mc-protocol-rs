cf2346a22e7c86567e86fc2919dde480f0f1871c
ShizukanaOtaku <otakushizukana@gmail.com>
2025-03-02 21:50:49 +0100

    First macro implementation, some weird bug with packet ids >:(

--- src/main.rs
+++ src/main.rs
@@ -9,37 +9,23 @@ use packet::{
     outbound::OutboundPacket,
     parse_packet,
 };
+use util::var_int::VarInt;
 
 const MAX_PACKET_SIZE: usize = 2097151;
-const SERVER_STATUS: &str = "
-                {
-                    \"version\": {
-                    \"name\": \"1.21.4\",
-                    \"protocol\": 769
-                },
-                \"players\": {
-                    \"max\": 64,
-                    \"online\": 8,
-                    \"sample\": [
-                        {
-                            \"name\": \"rustmc\",
-                            \"id\": \"4566e69f-c907-48ee-8d71-d7ba5aa00d20\"
-                        }
-                    ]
-                },
-                \"description\": {
-                    \"text\": \"Rust says hello! :3\"
-                },
-                    \"favicon\": \"data:image/png;base64,<data>\",
-                    \"enforcesSecureChat\": false
-                }
-";
+const SERVER_STATUS: &str = "{\"version\":{\"name\":\"1.21.4\",\"protocol\":769}}";
 
 mod packet;
 mod util;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:25565").unwrap();
+
+    let test = parse_packet(&vec![
+        44, 123, 34, 118, 101, 114, 115, 105, 111, 110, 34, 58, 123, 34, 110, 97, 109, 101, 34, 58,
+        34, 49, 46, 50, 49, 46, 52, 34, 44, 34, 112, 114, 111, 116, 111, 99, 111, 108, 34, 58, 55,
+        54, 57, 125, 125,
+    ]);
+    println!("{test:?}");
 
     for stream in listener.incoming() {
         let mut stream = stream.unwrap();
@@ -55,26 +41,31 @@ fn handle_connection(stream: &mut std::net::TcpStream) {
     let bytes_read = stream.read(&mut buf).unwrap();
     let buf = &buf[0..bytes_read];
     println!("Read {bytes_read} bytes: {buf:?}");
-    let packet = parse_packet(&buf.to_vec());
-    match InboundPacket::try_from(packet) {
-        Ok(packet_type) => match packet_type {
-            InboundPacket::HandshakePacket {
-                protocol_version,
-                server_address,
-                server_port,
-                next_state,
-            } => {
-                println!("A handshake was received: protocol: {protocol_version}, {server_address}:{server_port}, next_state: {next_state}");
-                let response = OutboundPacket::StatusResponsePacket {
-                    status_json: SERVER_STATUS.to_string(),
-                };
-                let bytes: Vec<u8> = response.into();
-                stream.write(bytes.as_slice()).unwrap();
-            }
-        },
+    let raw_packet = parse_packet(&buf.to_vec());
+    match InboundPacket::try_from(raw_packet) {
+        Ok(packet) => handle_packet(stream, packet),
         Err(error) => match error {
             PacketParseError::CorruptPacket => println!("Corrupt packet received."),
             PacketParseError::UnknownPacket { id } => println!("Unknown packet type: {id}"),
         },
+    }
+}
+
+fn handle_packet(stream: &mut std::net::TcpStream, packet: InboundPacket) {
+    match packet {
+        InboundPacket::HandshakePacket {
+            protocol_version,
+            server_address,
+            server_port,
+            next_state,
+        } => {
+            println!("A handshake was received: protocol: {protocol_version}, {server_address}:{server_port}, next_state: {next_state}");
+            let response = OutboundPacket::StatusResponsePacket {
+                status_json: SERVER_STATUS.to_string(),
+            };
+            let bytes: Vec<u8> = response.into();
+            println!("{bytes:?}");
+            stream.write(bytes.as_slice()).unwrap();
+        }
     }
 }
--- src/packet/outbound.rs
+++ src/packet/outbound.rs
@@ -1,28 +1,78 @@
-use crate::util::encode::encode_varint;
+use crate::util::var_int::VarInt;
 
-#[derive(Debug)]
-pub enum OutboundPacket {
-    StatusResponsePacket { status_json: String },
+pub trait IntoMCPacketData {
+    fn into_mc_data(&self) -> Vec<u8>;
 }
 
-impl Into<Vec<u8>> for OutboundPacket {
-    fn into(self) -> Vec<u8> {
-        match self {
-            OutboundPacket::StatusResponsePacket { status_json } => {
-                let mut buf = Vec::new();
-                buf.extend(encode_varint(status_json.len()));
-                buf.extend(status_json.bytes());
-
-                let mut final_buf = Vec::new();
-                let packet_id = encode_varint(0x00);
-                let len = buf.len() + packet_id.len();
-
-                final_buf.extend(encode_varint(len));
-                final_buf.extend(packet_id);
-                final_buf.extend(buf);
-
-                final_buf
-            }
-        }
+impl IntoMCPacketData for String {
+    fn into_mc_data(&self) -> Vec<u8> {
+        let length = VarInt::new(self.len());
+        let mut data = length.into_mc_data();
+        data.extend(self.as_bytes());
+        data
     }
 }
+
+impl IntoMCPacketData for u8 {
+    fn into_mc_data(&self) -> Vec<u8> {
+        vec![*self]
+    }
+}
+
+impl IntoMCPacketData for usize {
+    fn into_mc_data(&self) -> Vec<u8> {
+        self.to_le_bytes().to_vec()
+    }
+}
+
+impl IntoMCPacketData for i32 {
+    fn into_mc_data(&self) -> Vec<u8> {
+        self.to_le_bytes().to_vec()
+    }
+}
+
+macro_rules! implement_packets {
+    ($( $packet_id:literal $variant:ident { $( $field:ident : $ty:ty ),* } ),* ) => {
+        #[derive(Debug)]
+        pub enum OutboundPacket {
+            $( $variant { $( $field : $ty ),* }, )*
+        }
+
+        impl Into<Vec<u8>> for OutboundPacket {
+            fn into(self) -> Vec<u8> {
+                match self {
+                    $(
+                        OutboundPacket::$variant { $( $field ),* } => {
+                            let mut encoded_packet = Vec::new();
+                            let packet_id = VarInt::new($packet_id);
+                            $(
+                                encoded_packet.extend($field.into_mc_data());
+                            )*
+                            let packet_length = VarInt::new(encoded_packet.len() + packet_id.bytes());
+                            let mut final_packet = Vec::new();
+                            final_packet.extend(packet_length.into_mc_data());
+                            final_packet.extend(encoded_packet);
+                            final_packet
+                        }
+                    ),*
+                }
+            }
+        }
+
+        impl OutboundPacket {
+            fn id(&self) -> usize {
+                match self {
+                    $(
+                        OutboundPacket::$variant { .. } => {
+                            $packet_id
+                        }
+                    ),*
+                }
+            }
+        }
+    };
+}
+
+implement_packets!(0x00 StatusResponsePacket {
+    status_json: String
+});
