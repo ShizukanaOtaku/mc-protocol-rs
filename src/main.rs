e8a57f8acff627304e67e960f4cfa961b5d6244d
ShizukanaOtaku <otakushizukana@gmail.com>
2025-03-02 18:19:39 +0100

    Handle the handshake

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,6 @@ use std::{io::Read, net::TcpListener, thread};
 
 const MAX_PACKET_SIZE: usize = 2097151;
 
-#[derive(Debug)]
-struct Packet {
-    protocol: usize,
-    id: usize,
-    length: usize,
-    data: Vec<u8>,
-}
-
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:25565").unwrap();
 
@@ -26,9 +18,65 @@ fn handle_connection(stream: &mut std::net::TcpStream) {
     let mut buf = vec![0; MAX_PACKET_SIZE];
     let bytes_read = stream.read(&mut buf).unwrap();
     let buf = &buf[0..bytes_read];
-    println!("Sucessfully read {bytes_read} bytes: {buf:?}");
     let packet = parse_packet(&buf.to_vec());
-    println!("Parsed a packet: {packet:?}");
+    match Packet::from(&packet) {
+        Some(packet_type) => match packet_type {
+            Packet::HandshakePacket {
+                server_address,
+                server_port,
+                next_state,
+            } => println!("A handshake was received: {server_address}:{server_port}, next_state: {next_state}"), 
+        },
+        None => {
+            println!("Packet of id {} is not implemented yet.", packet.id);
+        }
+    }
+}
+
+#[derive(Debug)]
+struct RawPacket {
+    protocol: usize,
+    id: usize,
+    length: usize,
+    data: Vec<u8>,
+}
+
+#[derive(Debug)]
+enum Packet {
+    HandshakePacket {
+        server_address: String,
+        server_port: u16,
+        next_state: usize,
+    },
+}
+
+impl Packet {
+    pub fn from(raw_packet: &RawPacket) -> Option<Self> {
+        match raw_packet.id {
+            0 => {
+                let len = decode_varint(&raw_packet.data[0..5]).unwrap();
+
+                let shift = len.1;
+                let server_address =
+                    String::from_utf8(raw_packet.data[shift..shift + len.0].to_vec()).unwrap();
+
+                let server_port = decode_u16_bytes((
+                    raw_packet.data[shift + len.0],
+                    raw_packet.data[shift + len.0 + 1],
+                ));
+
+                let next_state = decode_varint(&raw_packet.data[shift + len.0 + 2..])
+                    .unwrap()
+                    .0;
+                Some(Self::HandshakePacket {
+                    server_address,
+                    server_port,
+                    next_state,
+                })
+            }
+            _ => None,
+        }
+    }
 }
 
 fn decode_varint(mut buf: &[u8]) -> Result<(usize, usize), ()> {
@@ -52,7 +100,11 @@ fn decode_varint(mut buf: &[u8]) -> Result<(usize, usize), ()> {
     Err(()) // Invalid VarInt (too many bytes)
 }
 
-fn parse_packet(buf: &Vec<u8>) -> Packet {
+fn decode_u16_bytes(bytes: (u8, u8)) -> u16 {
+    (bytes.0 as u16) << 8 | bytes.1 as u16
+}
+
+fn parse_packet(buf: &Vec<u8>) -> RawPacket {
     let mut shift = 0;
     let length = decode_varint(&buf[0..5]).unwrap();
     shift += length.1;
@@ -61,7 +113,7 @@ fn parse_packet(buf: &Vec<u8>) -> Packet {
     let protocol = decode_varint(&buf[shift..shift + 5]).unwrap();
     shift += protocol.1;
     let data = &buf[shift..];
-    Packet {
+    RawPacket {
         protocol: protocol.0,
         id: id.0,
         length: length.0,
