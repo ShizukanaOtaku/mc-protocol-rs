67a1a73c5949fb05c4b82aebecdcff412d79c020
ShizukanaOtaku <otakushizukana@gmail.com>
2025-03-16 23:17:30 +0100

    handle invalid packets in parse_packet

--- src/packet/mod.rs
+++ src/packet/mod.rs
@@ -23,29 +23,33 @@ pub struct RawPacket {
 
 #[allow(clippy::ptr_arg)] // Some packets may be greater than the stack allows, so using the heap
                           // is neccesary
-pub fn parse_packet(buf: &Vec<u8>) -> (RawPacket, usize) {
+pub fn parse_packet(buf: &Vec<u8>) -> Option<(RawPacket, usize)> {
+    if buf.len() == 0 {
+        return None;
+    }
+
     if buf[0] == 0xFE {
-        return (
+        return Some((
             RawPacket {
                 length: 1,
                 id: 0xFE,
                 data: Vec::new(),
             },
             49,
-        );
+        ));
     }
     let length = VarInt::from_mc_bytes(buf).unwrap();
     let mut shift = length.1;
     let length: isize = length.0.try_into().unwrap();
     let id = VarInt::from_mc_bytes(&buf[shift..]).unwrap();
     shift += id.1;
-    let data = &buf[shift..shift + length as usize];
-    (
+    let data = &buf[shift..shift + length as usize - 1];
+    Some((
         RawPacket {
             length,
             id: id.0.try_into().unwrap(),
             data: data.to_vec(),
         },
         length as usize + 1,
-    )
+    ))
 }
