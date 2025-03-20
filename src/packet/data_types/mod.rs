c5d725fc589e3f50202d9adc2c046ccbc229c107
ShizukanaOtaku <otakushizukana@gmail.com>
2025-03-20 15:06:29 +0100

    macro implementation for size type coding

--- src/packet/data_types/mod.rs
+++ src/packet/data_types/mod.rs
@@ -7,17 +7,31 @@ pub trait MCEncode {
     fn into_mc_data(self) -> Vec<u8>;
 }
 
-macro_rules! impl_size_types_encoding {
-    ($($t:ty),*) => {
-        $(impl MCEncode for $t {
-            fn into_mc_data(self) -> Vec<u8> {
-                self.to_le_bytes().to_vec()
+macro_rules! impl_size_types {
+    ($($type:ty),*) => {
+        $(
+            impl MCDecode for $type {
+                fn from_mc_bytes(bytes: &[u8]) -> Option<($type, usize)> {
+                    const TYPE_SIZE: usize = size_of::<$type>();
+                    if bytes.len() < TYPE_SIZE {
+                        return None;
+                    }
+
+                    let bytes: [u8; TYPE_SIZE] = bytes[..TYPE_SIZE].try_into().unwrap();
+                    Some((<$type>::from_be_bytes(bytes), TYPE_SIZE))
+                }
             }
-        })*
+
+            impl MCEncode for $type {
+                fn into_mc_data(self) -> Vec<u8> {
+                    self.to_le_bytes().to_vec()
+                }
+            }
+        )*
     };
 }
 
-impl_size_types_encoding!(i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64);
+impl_size_types!(i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64);
 
 impl MCDecode for bool {
     fn from_mc_bytes(bytes: &[u8]) -> Option<(Self, usize)>
@@ -159,75 +173,5 @@ impl MCEncode for Property {
         data.extend(self.1.into_mc_data());
         data.extend(self.2.into_mc_data());
         data
-    }
-}
-
-impl MCDecode for i8 {
-    fn from_mc_bytes(bytes: &[u8]) -> Option<(Self, usize)>
-    where
-        Self: Sized,
-    {
-        if bytes.is_empty() {
-            return None;
-        }
-
-        let bytes: [u8; 1] = bytes[..1].try_into().unwrap();
-        Some((i8::from_be_bytes(bytes), 1))
-    }
-}
-
-impl MCDecode for u8 {
-    fn from_mc_bytes(bytes: &[u8]) -> Option<(Self, usize)>
-    where
-        Self: Sized,
-    {
-        if bytes.is_empty() {
-            return None;
-        }
-
-        let bytes: [u8; 1] = bytes[..1].try_into().unwrap();
-        Some((u8::from_be_bytes(bytes), 1))
-    }
-}
-
-impl MCDecode for u16 {
-    fn from_mc_bytes(bytes: &[u8]) -> Option<(Self, usize)>
-    where
-        Self: Sized,
-    {
-        if bytes.len() < 2 {
-            return None;
-        }
-
-        let bytes: [u8; 2] = bytes[..2].try_into().unwrap();
-        Some((u16::from_be_bytes(bytes), 2))
-    }
-}
-
-impl MCDecode for i64 {
-    fn from_mc_bytes(bytes: &[u8]) -> Option<(Self, usize)>
-    where
-        Self: Sized,
-    {
-        if bytes.len() < 8 {
-            return None;
-        }
-
-        let bytes: [u8; 8] = bytes[..8].try_into().unwrap();
-        Some((i64::from_be_bytes(bytes), 8))
-    }
-}
-
-impl MCDecode for u128 {
-    fn from_mc_bytes(bytes: &[u8]) -> Option<(Self, usize)>
-    where
-        Self: Sized,
-    {
-        if bytes.len() < 16 {
-            return None;
-        }
-
-        let bytes: [u8; 16] = bytes[..16].try_into().unwrap();
-        Some((u128::from_be_bytes(bytes), 16))
     }
 }
